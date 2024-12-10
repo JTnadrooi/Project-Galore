7a7783f2e374f710c7b13c6f5e865d84e832c6dc
JTnadrooi <jtnadrooi@gmail.com>
2024-12-10 13:42:33 +0100

    require fix

--- data.lua
+++ data.lua
@@ -3,41 +3,14 @@ require("__ylib__/data")
 vgal = require("prototypes.lib.vgal")
 vgal.log("the *core-of-galore* has loaded.")
 
--- require("prototypes.other.catagories")
--- require("prototypes.other.technology")
--- require("prototypes.other.them")
--- require("prototypes.other.fluids")
--- require("prototypes.other.invalid")
 require("prototypes.subgroups")
 
--- require("prototypes.fluids.f-intermediates")
-require("prototypes.intermediates.oil-processing")
-
--- require("prototypes.buildings.belts")
--- require("prototypes.buildings.assembling")
--- require("prototypes.buildings.fluid-transport")
--- require("prototypes.buildings.deco")
--- require("prototypes.buildings.tiles")
--- require("prototypes.buildings.labs")
--- require("prototypes.buildings.inserters")
--- require("prototypes.buildings.buildings")
--- require("prototypes.buildings.b-other")
-
--- require("prototypes.car")
 require("prototypes.intermediates.intermediates")
 require("prototypes.intermediates.chemical")
 require("prototypes.intermediates.circuits")
--- require("prototypes.intermediates.fuel")
--- require("prototypes.intermediates.engines")
-
+require("prototypes.intermediates.engines")
+require("prototypes.intermediates.oil-processing")
 
 require("prototypes.rocket-parts")
--- require("prototypes.modules")
--- require("prototypes.raw-materials")
-
--- require("prototypes.alloy")
-
--- require("prototypes.mods.ksp")
--- require("prototypes.mods.bep")
 
 require("prototypes.science-packs")

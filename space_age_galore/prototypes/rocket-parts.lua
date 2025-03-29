ac479e9a29999f4de0836b3f76b568dd29fd2eed
JTnadrooi <jtnadrooi@gmail.com>
2025-03-29 17:07:02 +0100

    added "lithium-plate-rocket-part" recipe

--- space_age_galore/prototypes/rocket-parts.lua
+++ space_age_galore/prototypes/rocket-parts.lua
@@ -1,23 +1,22 @@
--- vgal.data.extend({
---     {
---         name = "carbon-fiber-rocket-part",
---         prefix = "vgal",
---         technology = "rocket-silo",
---         icons = vgal.icon.register {
---             vgal.icon.get("rocket-part"),
---             vgal.icon.get_in("carbon-fiber")
---         },
---         energy_required = 2,
---         ingredients = {
---             { "rocket-fuel",     1 },
---             { "processing-unit", 1 },
---             { "carbon-fiber",    2 }, -- 
---         },
---         results = {
---             { "rocket-part", 1 }, -- 12800
---         },
---         category = "rocket-building",
---     },
--- }, {
---     groups = { "vgal-rocket-parts" }
--- })
+vgal.data.extend({
+    {
+        name = "lithium-plate-rocket-part",
+        prefix = "vgal",
+        technology = "lithium-processing",
+        icons = vgal.icon.register {
+            vgal.icon.get("rocket-part"),
+            vgal.icon.get_in("lithium-plate")
+        },
+        energy_required = 3,
+        ingredients = {
+            { "rocket-fuel",     1 },
+            { "processing-unit", 1 },
+        },
+        results = {
+            { "rocket-part", 1 }, -- 12800
+        },
+        category = "rocket-building",
+    },
+}, {
+    groups = { "vgal-rocket-parts" }
+})

d24ff92339c80cbbc1471c85ff0a966e798a5f6f
JTnadrooi <jtnadrooi@gmail.com>
2025-04-10 19:59:04 +0200

    added space age hidden dep

--- galore_lib/data.lua
+++ galore_lib/data.lua
@@ -8,9 +8,14 @@ vgal.recipe.add_productivity_entry("iron-ore")
 vgal.recipe.add_productivity_entry("copper-ore")
 vgal.recipe.add_productivity_entry("uranium-ore")
 vgal.recipe.add_productivity_entry("stone")
-vgal.recipe.add_productivity_entry("ammonia")
-vgal.recipe.add_productivity_entry("light-oil")
-vgal.recipe.add_productivity_entry("heavy-oil")
-vgal.recipe.add_productivity_entry("petroleum-gas")
-vgal.recipe.add_productivity_entry("solid-fuel")
-vgal.recipe.add_productivity_entry("crude-oil")
+
+-- vgal.recipe.add_productivity_entry("light-oil")
+-- vgal.recipe.add_productivity_entry("heavy-oil")
+-- vgal.recipe.add_productivity_entry("petroleum-gas")
+-- vgal.recipe.add_productivity_entry("solid-fuel")
+-- vgal.recipe.add_productivity_entry("crude-oil")
+
+-- if mods["space-age"] then
+--     vgal.recipe.add_productivity_entry("ammonia")
+--     vgal.recipe.add_productivity_entry("ammoniacal-solution")
+-- end

0b9984aa89214130c0abbc1df5007d550cb2a735
JTnadrooi <jtnadrooi@gmail.com>
2025-04-11 07:58:44 +0200

    added "fluorine-kovarex-enrichment-process" recipe

--- space_age_galore/prototypes/intermediates/intermediates.lua
+++ space_age_galore/prototypes/intermediates/intermediates.lua
@@ -112,7 +112,7 @@ vgal.data.extend({
             vgal.icon.get_out_to("uranium-ore"),
             vgal.icon.get_overlay("to"),
         },
-        energy_required = 10,
+        energy_required = 25,
         technology = { "uranium-processing", "space-platform" },
         -- fluid_ingredients = {
         --     { "sulfuric-acid", 20 },
@@ -122,7 +122,7 @@ vgal.data.extend({
         },
         results = {
             { "uranium-ore", nil, { amount_min = 2, amount_max = 8 } }, -- avg; 5
-            { "carbon",      nil, { amount_min = 1, amount_max = 4 } }, -- avg; 2.5
+            { "carbon",      1,   { probability = 0.70 } },
         },
         category = "centrifuging",
         show_amount_in_title = false,
@@ -133,7 +133,7 @@ vgal.data.extend({
         icons = vgal.icon.register {
             vgal.icon.get_placeholder(),
         },
-        energy_required = 12,
+        energy_required = 10,
         technology = { "kovarex-enrichment-process", "cryogenic-plant" },
         fluid_ingredients = {
             { "fluorine", 40 },
@@ -144,6 +144,28 @@ vgal.data.extend({
         results = {
             { "uranium-235", 1, { probability = 0.033 } }, -- ~5 times as much U235!!!
             { "uranium-238", 1, { probability = 0.967 } },
+        },
+        category = "centrifuging",
+        show_amount_in_title = false,
+    },
+    {
+        name = "fluorine-kovarex-enrichment-process",
+        prefix = "vgal",
+        icons = vgal.icon.register {
+            vgal.icon.get_placeholder(),
+        },
+        energy_required = 30,
+        technology = { "kovarex-enrichment-process", "cryogenic-plant" },
+        fluid_ingredients = {
+            { "fluorine", 50 },
+        },
+        ingredients = {
+            { "uranium-235", 10, { ignored_by_stats = 10 } },
+            { "uranium-238", 7,  { ignored_by_stats = 6 } },
+        },
+        results = {
+            { "uranium-235", 11, { ignored_by_stats = 10, ignored_by_productivity = 10 } },
+            { "uranium-238", 6,  { ignored_by_stats = 6, ignored_by_productivity = 6 } },
         },
         category = "centrifuging",
         show_amount_in_title = false,

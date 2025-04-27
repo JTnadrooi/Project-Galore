639db218e5ab02270906a8f8376100d625d76dd7
JTnadrooi <jtnadrooi@gmail.com>
2025-04-27 20:31:06 +0200

    added ORE_INDEXES constant

--- angels_galore/constants.lua
+++ angels_galore/constants.lua
@@ -2,3 +2,4 @@ agal = {}
 agal.constants = {}
 agal.constants.ENVIRONMENTS = { "temperate", "swamp", "desert" }
 agal.constants.ORE_STATES = { "crushed", "crystal", "chunk", "pure" }
+agal.constants.ORE_INDEXES = { 1, 3 }

8aa03c84193299f23a5be7e6cefc9a56966cf755
JTnadrooi <jtnadrooi@gmail.com>
2025-04-27 15:35:04 +0200

    moved autoplace fixes to normal data stage

---
+++ angels_galore/overrides/angels-overrides.lua
@@ -0,0 +1,18 @@
+--- ore removal ---
+
+local TO_REMOVE = { "angels-ore2", "angels-ore4" }
+
+for _, ore in pairs(TO_REMOVE) do
+    data.raw["resource"][ore] = nil
+    data.raw["autoplace-control"][ore] = nil
+    data.raw["planet"]["nauvis"].map_gen_settings.autoplace_controls[ore] = nil
+    data.raw["planet"]["nauvis"].map_gen_settings.autoplace_settings.entity.settings[ore] = nil
+end
+
+for _, preset in pairs(data.raw["map-gen-presets"].default) do
+    if preset.basic_settings and preset.basic_settings.autoplace_controls then
+        for _, ore in pairs(TO_REMOVE) do
+            preset.basic_settings.autoplace_controls[ore] = nil
+        end
+    end
+end

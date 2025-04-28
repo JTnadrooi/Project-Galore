7e02830efde0bff587811cf0cfe6ff3362177894
JTnadrooi <jtnadrooi@gmail.com>
2025-04-28 12:30:04 +0200

    fixed agal.constants.ORE_STATES order

--- angels_galore/constants.lua
+++ angels_galore/constants.lua
@@ -1,5 +1,5 @@
 agal = {}
 agal.constants = {}
 agal.constants.ENVIRONMENTS = { "temperate", "swamp", "desert" }
-agal.constants.ORE_STATES = { "crushed", "crystal", "chunk", "pure" }
+agal.constants.ORE_STATES = { "crushed", "chunk", "crystal", "pure" }
 agal.constants.ORE_INDEXES = { 1, 3 }

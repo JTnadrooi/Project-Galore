26740a393a3eb1056296119855bb5d2341dec7b7
JTnadrooi <jtnadrooi@gmail.com>
2025-04-29 19:40:29 +0200

    "garden-cultivating" icon fix

--- angels_galore/overrides/angels-icon-overrides.lua
+++ angels_galore/overrides/angels-icon-overrides.lua
@@ -6,3 +6,10 @@ data.raw.recipe["copper-ore-processing"].icons = vgal.icon.register({
     vgal.icon.get("processed-copper"),
     vgal.icon.get_in("copper-ore"),
 })
+
+data.raw.recipe["garden-cultivating"].icons = vgal.icon.register({
+    vgal.icon.get("paste-cellulose"),
+    vgal.icon.get_out("temperate-garden"),
+    vgal.icon.get_out2("desert-garden"),
+    vgal.icon.get_out3("swamp-garden"),
+})

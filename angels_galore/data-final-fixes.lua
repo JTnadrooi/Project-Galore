c88ad054603a538c880499bb0d0dd15e6f905eac
JTnadrooi <jtnadrooi@gmail.com>
2025-04-30 11:10:22 +0200

    set all tech.unit.time to 5

--- angels_galore/data-final-fixes.lua
+++ angels_galore/data-final-fixes.lua
@@ -1,2 +1,12 @@
 require("overrides.angels-overrides-final-fixes")
 
+for _, tech in pairs(data.raw.technology) do
+    if tech.unit then
+        -- for index, value in ipairs(tech.unit.ingredients or {}) do
+        --     if value[1] == "military-science-pack" then
+        --         table.remove(tech.unit.ingredients, index)
+        --     end
+        -- end
+        tech.unit.time = 5
+    end
+end

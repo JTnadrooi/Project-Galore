44ff1885a7efd0ac28c8d9d4780f502c8c1dc345
JTnadrooi <jtnadrooi@gmail.com>
2025-04-30 11:58:15 +0200

    added belt overrides

--- angels_galore/data.lua
+++ angels_galore/data.lua
@@ -4,3 +4,7 @@ require("prototypes.subgroups")
 require("overrides.angels-prod-overrides")
 require("overrides.angels-recipe-overrides")
 require("overrides.angels-overrides")
+
+data.raw["underground-belt"]["underground-belt"].max_distance = 8
+data.raw["underground-belt"]["fast-underground-belt"].max_distance = 10
+data.raw["underground-belt"]["express-underground-belt"].max_distance = 12

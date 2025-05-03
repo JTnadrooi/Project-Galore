43eaf6206d4e350faf2ff76f87b8e90a0098a977
JTnadrooi <jtnadrooi@gmail.com>
2025-05-03 19:13:30 +0200

    made it so ineffective Vanilla Galore settings get hidden

--- space_age_galore/settings.lua
+++ space_age_galore/settings.lua
@@ -1,2 +1,5 @@
 require("prototypes.settings.groups")
 require("prototypes.settings.other-settings")
+
+data.raw["bool-setting"]["vgal-renewable-ores"].hidden = true
+data.raw["bool-setting"]["vgal-renewable-ores"].forced_value = false

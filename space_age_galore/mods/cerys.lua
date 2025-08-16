92369c9272722b66f5fae8ddb51e9b3fea81af8b
JTnadrooi <jtnadrooi@gmail.com>
2025-08-16 15:03:22 +0200

    remove debug comment

--- space_age_galore/mods/cerys.lua
+++ space_age_galore/mods/cerys.lua
@@ -12,7 +12,6 @@ for _, recipe in vgal.data.domain_pairs("vgal", "recipe") do
             if data.raw["technology"][tech_key] then
                 vgal.tech.add_productivity_change(tech_key, recipe.name)
             else
-                -- error(tech_pre_name)
                 break
             end
         end

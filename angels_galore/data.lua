974c4c0acb1e0f815248f5440cc93a085d922a90
JTnadrooi <jtnadrooi@gmail.com>
2026-04-07 19:26:44 +0200

    Move vanilla overrides to vanilla-overrides.lua file + Increase small-electric-pole wire distance by one

--- angels_galore/data.lua
+++ angels_galore/data.lua
@@ -2,13 +2,5 @@ require("lib.agal")
 
 require("prototypes.subgroups")
 
-data.raw["underground-belt"]["underground-belt"].max_distance = 8
-data.raw["underground-belt"]["fast-underground-belt"].max_distance = 10
-data.raw["underground-belt"]["express-underground-belt"].max_distance = 12
-
-for _, inserter in pairs(data.raw["inserter"]) do
-    inserter.stack_size_bonus = (inserter.stack_size_bonus or 0) + 2
-end
-
 require("prototypes.metal-both")
 require("prototypes.sorting")
---
+++ angels_galore/overrides/vanilla-overrides.lua
@@ -0,0 +1,14 @@
+-- there are also some vanilla overrides in loot-overrides.
+
+--- increase underground belt distances --- (seems only fair with the huge new machines)
+data.raw["underground-belt"]["underground-belt"].max_distance = 8
+data.raw["underground-belt"]["fast-underground-belt"].max_distance = 10
+data.raw["underground-belt"]["express-underground-belt"].max_distance = 12
+
+--- increase power reach --- (huge machines, looking at sorting machine rn)
+data.raw["electric-pole"]["small-electric-pole"].maximum_wire_distance = 8.5 -- 1 tile increase for the sorting machine. (also makes it reach past ec miner + belt + ec miner)
+
+--- increase inserter hand sizes --- (lots of new fast outputting recipes alsooooo)
+for _, inserter in pairs(data.raw["inserter"]) do
+    inserter.stack_size_bonus = (inserter.stack_size_bonus or 0) + 2
+end

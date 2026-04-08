7f5aeccd7c2ab8b6b8b65e90ae659979ffc52bfe
JTnadrooi <jtnadrooi@gmail.com>
2026-04-08 08:58:04 +0200

    Remove 5 tech unit time for all techs

--- angels_galore/data-final-fixes.lua
+++ angels_galore/data-final-fixes.lua
@@ -1,19 +1,13 @@
 require("overrides.angels-overrides-final-fixes")
 
-for _, tech in pairs(data.raw.technology) do
-    if tech.unit then
-        -- for index, value in ipairs(tech.unit.ingredients or {}) do
-        --     if value[1] == "military-science-pack" then
-        --         table.remove(tech.unit.ingredients, index)
-        --     end
-        -- end
-        tech.unit.time = 5
-    end
-end
-
--- data.raw["recipe"]["copper-ore-smelting"].order = "da"
--- data.raw["recipe"]["processed-copper-smelting"].order = "db"
--- data.raw["recipe"]["anode-copper-smelting"].order = "dc"
--- data.raw["recipe"]["iron-ore-smelting"].order = "ca"
--- data.raw["recipe"]["processed-iron-smelting"].order = "cb"
--- data.raw["recipe"]["pellet-iron-smelting"].order = "cc"
+-- commentedbc: big lab setups ftw
+-- for _, tech in pairs(data.raw.technology) do
+--     if tech.unit then
+--         -- for index, value in ipairs(tech.unit.ingredients or {}) do
+--         --     if value[1] == "military-science-pack" then
+--         --         table.remove(tech.unit.ingredients, index)
+--         --     end
+--         -- end
+--         tech.unit.time = 5
+--     end
+-- end

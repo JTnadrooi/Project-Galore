56b0ddfdaf136b7674e7fc9f1a406b11cf9dc577
JTnadrooi <jtnadrooi@gmail.com>
2026-04-08 10:33:49 +0200

    Use vgal.setting function instead of local

--- angels_galore/settings.lua
+++ angels_galore/settings.lua
@@ -1,18 +1,13 @@
-require("lib.agal") -- I kinda need my constants..
+require("lib.constants") -- I kinda need my constants..
 
-local function hide_and_force(name, forced_value)
-    data.raw["bool-setting"][name].hidden = true
-    data.raw["bool-setting"][name].forced_value = forced_value
-end
-
-hide_and_force("vgal-renewable-ores")
-hide_and_force("vgal-oil-recipes")
-hide_and_force("vgal-oil-recipes")
+vgal.setting.hide_and_force("vgal-renewable-ores")
+vgal.setting.hide_and_force("vgal-oil-recipes")
+vgal.setting.hide_and_force("vgal-oil-recipes")
 
 for _, ore_index in ipairs(agal.constants.REMOVED_ORE_INDEXES) do
-    hide_and_force("angels-starting-resource-ore" .. ore_index, true) -- Dissabling is done in final fixes, I need a autoplace to copy it to ore2, if I say false, it wont show up in starter patch
+    vgal.setting.hide_and_force("angels-starting-resource-ore" .. ore_index, true) -- Dissabling is done in final fixes, I need a autoplace to copy it to ore2, if I say false, it wont show up in starter patch
 end
 
 for _, ore_index in ipairs(agal.constants.ORE_INDEXES) do
-    hide_and_force("angels-starting-resource-ore" .. ore_index, true)
+    vgal.setting.hide_and_force("angels-starting-resource-ore" .. ore_index, true)
 end
--- galore_lib/lib/setting.lua
+++ galore_lib/lib/setting.lua
@@ -13,3 +13,21 @@ function vgal.setting.new_bool(name, order, default_value)
         order = order
     }
 end
+
+function vgal.setting.new_string(name, order, default_value)
+    if default_value == nil then
+        default_value = ""
+    end
+    return {
+        type = "string-setting",
+        name = name,
+        setting_type = "startup",
+        default_value = default_value,
+        order = order
+    }
+end
+
+function vgal.setting.hide_and_force(name, forced_value)
+    data.raw["bool-setting"][name].hidden = true
+    data.raw["bool-setting"][name].forced_value = forced_value
+end

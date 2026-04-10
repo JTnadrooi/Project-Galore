b25e2b93d58d11c2723d8df3750f4b1cb0e47afe
JTnadrooi <jtnadrooi@gmail.com>
2026-04-10 10:44:43 +0200

    Add hide_and_force forced value

--- angels_galore/settings.lua
+++ angels_galore/settings.lua
@@ -1,8 +1,7 @@
 require("lib.constants") -- I kinda need my constants..
 
-vgal.setting.hide_and_force("vgal-renewable-ores")
-vgal.setting.hide_and_force("vgal-oil-recipes")
-vgal.setting.hide_and_force("vgal-oil-recipes")
+vgal.setting.hide_and_force("vgal-renewable-ores", false)
+vgal.setting.hide_and_force("vgal-oil-recipes", false)
 
 for _, ore_index in ipairs(agal.constants.REMOVED_ORE_INDEXES) do
     vgal.setting.hide_and_force("angels-starting-resource-ore" .. ore_index, true) -- Dissabling is done in final fixes, I need a autoplace to copy it to ore2, if I say false, it wont show up in starter patch

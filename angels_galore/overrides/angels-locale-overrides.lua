b18e1d30ad18bb5bf104c3fc3cf15848f33f7b77
JTnadrooi <jtnadrooi@gmail.com>
2026-04-21 12:38:04 +0200

    Remove sorting descs referencing removes ores

--- angels_galore/overrides/angels-locale-overrides.lua
+++ angels_galore/overrides/angels-locale-overrides.lua
@@ -2,4 +2,13 @@ for _, metal in pairs(vgal.defines.metals) do
     -- vgal.recipe.use_recipe_locale(metal.ingot)
     vgal.recipe.use_recipe_locale(metal.ingot .. "-2")
     vgal.recipe.use_recipe_locale(metal.ingot .. "-3")
+
+    -- remove sorting descs referencing removes ores
+    data.raw["item"][metal.base_ore].localised_description = { "item-description." .. metal.base_ore }
+    for _, state in ipairs(metal.ore_states) do
+        data.raw["item"][metal.base_ore .. "-" .. state].localised_description =
+        {
+            "item-description." .. metal.base_ore .. "-" .. state,
+        }
+    end
 end

ad61642ff05ae2edc0ac2fdd8d64ccca7a15f7b4
JTnadrooi <jtnadrooi@gmail.com>
2026-04-22 15:56:19 +0200

    Add "vgal-angels-solid-leafs-angels-cellulose-fiber" recipe

---
+++ angels_galore/prototypes/bio.lua
@@ -0,0 +1,23 @@
+-- to make costs align with vanilla (one plastic bar should be around as much "worth" as 1.5 plates) plastic bar recipes have been buffed
+vgal.data.extend({
+    {
+        name = "angels-solid-leafs-angels-cellulose-fiber",
+        prefix = "vgal",
+        icons = vgal.icon.register {
+            vgal.icon.get("angels-cellulose-fiber"),
+            vgal.icon.get_in("angels-solid-leafs"),
+        },
+        energy_required = 1,
+        technology = "angels-bio-arboretum-1",
+        ingredients = {
+            { "angels-solid-leafs", 10 },
+        },
+        results = {
+            { "angels-cellulose-fiber", 2 },
+        },
+        show_amount_in_title = false,
+        category = "crafting",
+    }
+}, {
+    type = "recipe",
+})

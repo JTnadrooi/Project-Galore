254408b9c43ecd06260d20e1310a4229a359f632
JTnadrooi <jtnadrooi@gmail.com>
2026-04-24 16:03:14 +0200

    Use link_ingredient for DemolisherScales compat

--- space_age_galore/mods/demolisher-scales.lua
+++ space_age_galore/mods/demolisher-scales.lua
@@ -1,7 +1,3 @@
 if not mods["DemolisherScales"] then return end
 
-for _, recipe in vgal.data.domain_pairs("vgal", "recipe") do
-    if recipe.main_product == "big-mining-drill" then
-        table.insert(recipe.ingredients, { type = "item", name = "demolisher-scale", amount = 1 })
-    end
-end
+vgal.recipe.all.link_ingredient({ type = "item", name = "demolisher-scale", amount = 1 }, "big-mining-drill", 1, "vgal")

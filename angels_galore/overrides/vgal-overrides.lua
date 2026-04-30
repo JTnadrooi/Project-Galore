07e8d6aa36f1c532deaec54f1d3438531d108836
JTnadrooi <jtnadrooi@gmail.com>
2026-04-30 17:50:52 +0200

    Remove out-of-placeified vgal recipes

--- angels_galore/overrides/vgal-overrides.lua
+++ angels_galore/overrides/vgal-overrides.lua
@@ -1,4 +1,4 @@
--- trim unsavable recipes.
+-- trim unsavable recipes
 vgal.data.trim("vgal-petroleum-gas-rocket-fuel")
 vgal.data.trim("vgal-plastic-bar-lubricant")
 vgal.data.trim("vgal-coal-sulfur")
@@ -8,20 +8,6 @@ vgal.data.trim("vgal-iron-stick-concrete")
 vgal.data.trim("vgal-copper-cable-plastic-bar")
 vgal.data.trim("vgal-crude-oil-coal")
 
--- update savable recipes
-data.raw["recipe"]["vgal-coal-uranium-235-nuclear-fuel"].order = "j"
-data.raw.recipe["vgal-coal-uranium-235-nuclear-fuel"].ingredients = vgal.build.table({
-    { "angels-rocket-fuel-capsule", 1 },
-    { "uranium-235",                1 },
-    { "angels-solid-carbon",        5 },
-})
-
-data.raw.recipe["vgal-coal-uranium-235-nuclear-fuel"].icons = vgal.icon.register {
-    vgal.icon.get("nuclear-fuel"),
-    vgal.icon.get_in("angels-solid-carbon"),
-}
-
-data.raw["recipe"]["vgal-steel-plate-iron-stick"].icons = vgal.icon.register {
-    vgal.icon.get("iron-stick"),
-    vgal.icon.get_in("steel-plate"),
-}
+-- trim out-of-placeified recipes
+vgal.data.trim("vgal-coal-uranium-235-nuclear-fuel")
+vgal.data.trim("vgal-low-density-structure-uranium-fuel-cell")

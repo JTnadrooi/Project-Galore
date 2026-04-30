8192de596c3e41cb54e6b0294344b0d55a6ba105
JTnadrooi <jtnadrooi@gmail.com>
2026-04-30 17:47:31 +0200

    Remove crash site overrides

--- angels_galore/control.lua
+++ angels_galore/control.lua
@@ -4,49 +4,10 @@ local function update_disco_science()
     end
 end
 
-local function fix_crash_site()
-    for _, surface in pairs(game.surfaces) do -- just in case I forget to fix it when I eventually add space age support and someone starts on another surface.. just in case...
-        function process_entity(entity_name)
-            local new_name = "vgal-" .. entity_name
-            local entities = surface.find_entities_filtered { position = { 0, 0 }, radius = 200, name = entity_name }
-            for _, ent in pairs(entities) do
-                local position = ent.position
-                ent.destroy()
-                surface.create_entity {
-                    name = new_name,
-                    position = position
-                }
-            end
-        end
-
-        for _, size in pairs({ "small", "medium", "big" }) do
-            for i = 1, 6 do
-                local original_name = "crash-site-spaceship-wreck-" .. size .. "-" .. i
-
-                if prototypes.entity[original_name] then
-                    process_entity(original_name)
-                end
-            end
-        end
-
-        process_entity("crash-site-spaceship")
-    end
-    game.print("All crash site wrecks replaced with vgal versions.")
-end
-
 script.on_configuration_changed(function()
     update_disco_science()
 end)
 
 script.on_init(function()
-    script.on_event(defines.events.on_tick, function(event) -- just on_init doesn't work.
-        fix_crash_site()
-        script.on_event(defines.events.on_tick, nil)
-    end)
-
     update_disco_science()
-end)
-
-commands.add_command("vgal-fix-crash-site", "Replaces crash site entities with the vgal versions.", function()
-    fix_crash_site()
 end)

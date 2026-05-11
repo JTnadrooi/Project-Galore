ccaa3bea8317c150ac3ebf4af1ae3bbc865b4ecf
JTnadrooi <jtnadrooi@gmail.com>
2026-05-11 18:07:08 +0200

    Fix quality mod not using updated recipes

--- angels_galore/data-final-fixes.lua
+++ angels_galore/data-final-fixes.lua
@@ -1,15 +1,5 @@
 require("overrides.angels-overrides-final-fixes")
--- commentedbc: big lab setups ftw
--- for _, tech in pairs(data.raw.technology) do
---     if tech.unit then
---         -- for index, value in ipairs(tech.unit.ingredients or {}) do
---         --     if value[1] == "military-science-pack" then
---         --         table.remove(tech.unit.ingredients, index)
---         --     end
---         -- end
---         tech.unit.time = 5
---     end
--- end
+require("mods.quality")
 
 -- because this doesnt listen in subgroups and im not putting that in final fixes
 data.raw["item"]["chemical-plant"].subgroup = "vgal-chemistry-machines"

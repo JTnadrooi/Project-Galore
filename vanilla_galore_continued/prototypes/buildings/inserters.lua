92f7460b43d14155bdbdf4088de6cc0e5500690b
JTnadrooi <jtnadrooi@gmail.com>
2026-05-17 16:41:15 +0200

    Add burner-inserter-inserter recipe

--- vanilla_galore_continued/prototypes/buildings/inserters.lua
+++ vanilla_galore_continued/prototypes/buildings/inserters.lua
@@ -1,40 +1,20 @@
 vgal.data.extend({
-    -- {
-    --     name = "copper-plate-inserter",
-    --     prefix = "vgal",
-    --     icons = vgal.icon.register {
-    --         vgal.icon.get("inserter"),
-    --         vgal.icon.get_in("copper-plate"),
-    --     },
-    --     technology = "electronics",
-    --     energy_required = 1,
-    --     ingredients = {
-    --         { "electronic-circuit", 2 }, -- 50
-    --         { "copper-cable",       6 }, -- 30
-    --         { "copper-plate",       1 }, -- 10
-    --     },
-    --     results = {
-    --         { "inserter", 2 }, -- 110
-    --     },
-    -- },
-    -- {
-    --     name = "advanced-circuit-fast-inserter",
-    --     prefix = "vgal",
-    --     icons = vgal.icon.register {
-    --         vgal.icon.get("fast-inserter"),
-    --         vgal.icon.get_in("advanced-circuit"),
-    --     },
-    --     technology = "advanced-circuit",
-    --     energy_required = 1,
-    --     ingredients = {
-    --         { "advanced-circuit", 1 }, -- 100
-    --         { "iron-gear-wheel",  1 }, -- 20
-    --         { "inserter",         2 }, -- 110
-    --     },
-    --     results = {
-    --         { "fast-inserter", 2 }, -- 230
-    --     },
-    -- },
+    {
+        name = "burner-inserter-inserter",
+        prefix = "vgal",
+        icons = vgal.icon.register {
+            vgal.icon.get("inserter"),
+            vgal.icon.get_in("burner-inserter"),
+        },
+        energy_required = 1,
+        ingredients = {
+            { "burner-inserter",    1 },
+            { "electronic-circuit", 1 },
+        },
+        results = {
+            { "inserter", 1 },
+        },
+    },
     {
         name = "processing-unit-bulk-inserter",
         prefix = "vgal",

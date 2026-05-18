84d886a6bde2326b3d7d64df2a24b27f3251b30e
JTnadrooi <jtnadrooi@gmail.com>
2026-05-18 07:54:23 +0200

    Make override-vanilla-crafting-speeds default to true

--- angels_galore/prototypes/settings/other-settings.lua
+++ angels_galore/prototypes/settings/other-settings.lua
@@ -1,4 +1,4 @@
 data.extend({
-    vgal.setting.new_bool("vgal-override-vanilla-crafting-speeds", "a", false),
+    vgal.setting.new_bool("vgal-override-vanilla-crafting-speeds", "a", true),
     vgal.setting.new_bool("vgal-remove-steel-intermediate-crafting", "b", true),
 })

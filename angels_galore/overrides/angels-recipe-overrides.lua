b4e9af3465808cd27dd64aebed9203fb66fbb831
JTnadrooi <jtnadrooi@gmail.com>
2026-05-23 18:33:29 +0200

    Fix too long steel roll casting speeds

--- angels_galore/overrides/angels-recipe-overrides.lua
+++ angels_galore/overrides/angels-recipe-overrides.lua
@@ -13,6 +13,8 @@ end
 data.raw.recipe["angels-plate-steel"].energy_required = 2
 vgal.recipe.multiply("angels-plate-steel", 1.25)
 
+data.raw.recipe["angels-roll-steel"].energy_required = 1
+
 vgal.recipe.replace_ingredient("nuclear-fuel", "rocket-fuel", "angels-rocket-fuel-capsule")
 vgal.recipe.replace_ingredient("angels-nuclear-fuel", "rocket-fuel", "angels-rocket-fuel-capsule")
 vgal.recipe.replace_ingredient("angels-nuclear-fuel-2", "rocket-fuel", "angels-rocket-fuel-capsule")

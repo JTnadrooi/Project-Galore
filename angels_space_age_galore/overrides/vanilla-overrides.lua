36c2fdb281388b039e96c4d058f3a699dd70967d
JTnadrooi <jtnadrooi@gmail.com>
2026-05-27 10:46:56 +0200

    Foundry fixes

--- angels_space_age_galore/overrides/vanilla-overrides.lua
+++ angels_space_age_galore/overrides/vanilla-overrides.lua
@@ -23,4 +23,31 @@ vgal.data.trim("burnt-spoilage")
 vgal.data.trim("fish-breeding")
 
 -- remove solid fuel from scrap
+-- no throw bc other mods like to do this as well
 vgal.recipe.remove_result("scrap-recycling", "solid-fuel", true)
+
+-- foundry fixes
+do
+    local foundry = data.raw["assembling-machine"]["foundry"]
+
+    -- first reduce crafting speed
+    -- why? so I can use the base angels galore casting recipes without adding new slower ones (that would be as fast as the game) and/or make the casting machine speeds weird
+    -- 4 is way to fast anyways....
+    foundry.crafting_speed = 2 -- og; 4
+
+    -- allow angels casting
+    table.insert(foundry.crafting_categories, "angels-casting")
+    table.insert(foundry.crafting_categories, "angels-strand-casting")
+
+    -- fix foundry recipes
+    -- data.raw["recipe"]["foundry"].energy_required = 5                  -- og; 10
+    -- data.raw["recipe"]["big-mining-drill"].energy_required = 15        -- og; 30
+    -- data.raw["recipe"]["metallurgic-science-pack"].energy_required = 5 -- og; 10
+    -- data.raw["recipe"]["tungsten-plate"].energy_required = 5           -- og; 10
+    for _, recipe in pairs(data.raw["recipe"]) do
+        if recipe.energy_required and recipe.energy_required >= 8
+            and (not (vgal.recipe.has_category(recipe.name, "crafting") or vgal.recipe.has_category(recipe.name, "crafting-with-fluid") or vgal.recipe.has_category(recipe.name, "pressing")) and vgal.recipe.has_category(recipe.name, "metallurgy")) then
+            recipe.energy_required = recipe.energy_required / 2
+        end
+    end
+end

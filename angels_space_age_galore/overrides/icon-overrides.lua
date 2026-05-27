459fa0b4c9c8ed63fc5792862ccfce216d131556
JTnadrooi <jtnadrooi@gmail.com>
2026-05-27 15:59:05 +0200

    Fix molten metals in icons

--- angels_space_age_galore/overrides/icon-overrides.lua
+++ angels_space_age_galore/overrides/icon-overrides.lua
@@ -6,3 +6,27 @@ data.raw["item"]["holmium-plate"].icon_size = 32
 
 data.raw["item"]["lithium-plate"].icon = "__angelssmeltinggraphics__/graphics/icons/plate-silver.png"
 data.raw["item"]["lithium-plate"].icon_size = 32
+
+data.raw.recipe["molten-iron-from-lava"].icons = vgal.icon.register({
+    vgal.icon.get("angels-liquid-molten-iron"),
+    vgal.icon.get_in("lava"),
+})
+data.raw.recipe["molten-copper-from-lava"].icons = vgal.icon.register({
+    vgal.icon.get("angels-liquid-molten-copper"),
+    vgal.icon.get_in("lava"),
+})
+
+data.raw.recipe["casting-low-density-structure"].icons = vgal.icon.register({
+    vgal.icon.get("low-density-structure"),
+    vgal.icon.get_in("angels-liquid-molten-copper"),
+    vgal.icon.get_in("angels-liquid-molten-steel"),
+})
+data.raw.recipe["vgal-molten-iron-rail"].icons = vgal.icon.register({
+    vgal.icon.get("rail"),
+    vgal.icon.get_in("angels-liquid-molten-steel"),
+})
+data.raw.recipe["vgal-molten-copper-carbon-fiber-low-density-structure"].icons = vgal.icon.register({
+    vgal.icon.get("low-density-structure"),
+    vgal.icon.get_in("carbon-fiber"),
+    vgal.icon.get_in2("angels-liquid-molten-copper"),
+})

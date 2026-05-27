3709cf088ce21bb27441ffc5def508b3cc3780e3
JTnadrooi <jtnadrooi@gmail.com>
2026-05-27 13:03:11 +0200

    Remove random assert

--- galore_lib/lib/table.lua
+++ galore_lib/lib/table.lua
@@ -110,7 +110,6 @@ end
 --- @param list table The table to remove the value from.
 --- @return table The table after removing the value.
 function vgal.table.remove(list, value)
-    assert(type(list) == "table", "'list' must be a table.")
     for i = #list, 1, -1 do
         if list[i] == value then
             table.remove(list, i)

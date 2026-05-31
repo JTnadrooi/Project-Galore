0e04bc490e831c433cb70de724adf0f5032e9ae6
JTnadrooi <jtnadrooi@gmail.com>
2026-05-31 16:17:16 +0200

    Squashed commit filled with prod fixes

--- angels_galore/overrides/angels-prod-overrides.lua
+++ angels_galore/overrides/angels-prod-overrides.lua
@@ -1,3 +1,6 @@
+-- only override file that runs in the datastage, why? so prod/catalyst entries get registered before any recipes get added.
+-- recipes decide what items get prod and what not according to the catalyst and prod entry lists
+
 -- register catalysts (catalysts don't work with productivity)
 vgal.recipe.add_catalyst_entry("angels-electrode-used")
 vgal.recipe.add_catalyst_entry("angels-liquid-coolant-used")
@@ -12,22 +15,29 @@ vgal.recipe.add_catalyst_entry("angels-liquid-polluted-fish-atmosphere")
 vgal.recipe.add_catalyst_entry("angels-solid-saw")
 vgal.recipe.add_catalyst_entry("angels-solid-crystal-tipped-saw")
 vgal.recipe.add_catalyst_entry("angels-solid-crystal-full-saw")
+
 vgal.recipe.add_catalyst_entry("angels-water-heavy-mud")
 vgal.recipe.add_catalyst_entry("angels-water-concentrated-mud")
 vgal.recipe.add_catalyst_entry("angels-water-thin-mud")
 vgal.recipe.add_catalyst_entry("angels-water-light-mud")
 vgal.recipe.add_catalyst_entry("angels-water-saline")
 
-vgal.recipe.add_catalyst_entry("raw-fish")
-for fish_index = 1, 3 do vgal.recipe.add_catalyst_entry("angels-alien-fish-" .. fish_index .. "-raw") end
-for puffer_index = 1, 5 do vgal.recipe.add_catalyst_entry("angels-bio-puffer-" .. puffer_index) end
+-- for puffer_index = 1, 5 do
+--     vgal.recipe.add_catalyst_entry("angels-bio-puffer-" .. puffer_index)
+-- end
+
+-- vgal.recipe.add_catalyst_entry("raw-fish")
+-- for fish_index = 1, 3 do vgal.recipe.add_catalyst_entry("angels-alien-fish-" .. fish_index .. "-raw") end
+
+-- for puffer_index = 1, 5 do vgal.recipe.add_catalyst_entry("angels-bio-puffer-" .. puffer_index) end
+
 for i = 3, 5 do
     for _, environment in pairs(agal.defines.environments) do
         vgal.recipe.add_catalyst_entry("angels-" .. environment.name .. "-" .. i .. "-seed-dormant")
     end
 end
 
--- machines that can benefit from productivity, no corrections needed (well except for the angels-bio-hatchery stuff).
+-- machines that can benefit from productivity, no corrections needed
 local productivity_machines = {
     "angels-bio-press", "angels-bio-generator-temperate-1",
     "angels-crop-farm", "angels-temperate-farm", "angels-swamp-farm", "angels-desert-farm", "angels-composter",
@@ -48,51 +58,34 @@ local productivity_machines = {
     "angels-gas-refinery-small", "angels-gas-refinery", "angels-separator",
 }
 
--- will be filled with crafting categories that should benefit from productivity.
+-- will be filled with crafting categories that should benefit from productivity
 local productivity_categories = {}
 
 -- make sure machines allow productivity.
 for _, machine_name in ipairs(productivity_machines) do
     local machine = data.raw["assembling-machine"][machine_name] or data.raw["furnace"][machine_name]
     if not machine then
-        error(machine_name)
+        error("Machine does not exist: " .. machine_name)
     end
     for i = 2, 6 do -- bc categories only get fixed in update subphase.
-        local machine_max_try = data.raw["assembling-machine"][machine_name .. "-" .. i] or
+        local max_tiered_machine = data.raw["assembling-machine"][machine_name .. "-" .. i] or
             data.raw["furnace"][machine_name .. "-" .. i]
-        if machine_max_try then machine = machine_max_try end
+        if max_tiered_machine then machine = max_tiered_machine end
     end
     local categories = machine.crafting_categories or {}
     for _, category in ipairs(categories) do
         productivity_categories[category] = machine_name
     end
-    machine.allowed_effects = { "speed", "productivity", "consumption", "pollution", "quality" }
+    machine.allowed_effects = { "speed", "productivity", "consumption", "pollution", "quality" } -- might be a bit too hard-codey
 end
 
 for _, recipe in pairs(data.raw["recipe"]) do
     if productivity_categories[recipe.category] then
-        if recipe.allow_productivity ~= false then
-            if productivity_categories[recipe.category] == "angels-bio-hatchery" then
-                vgal.recipe.smart_allow_productivity(recipe.name, true)
-                vgal.recipe.disallow_productivity_for_result(recipe.name, "angels-bio-puffer-egg-shell")
-            else
-                vgal.recipe.smart_allow_productivity(recipe.name)
-            end
+        if agal.is_angels(recipe) then
+            vgal.recipe.smart_allow_productivity(recipe.name)
         end
     end
 end
-
--- vgal.recipe.add_productivity_entry("liquid-plastic")
--- vgal.recipe.add_productivity_entry("rocket-booster")
--- vgal.recipe.add_productivity_entry("solid-carbon")
--- vgal.recipe.add_productivity_entry("cellolose-fiber")
--- vgal.recipe.add_productivity_entry("red-cellolose-fiber")
--- vgal.recipe.add_productivity_entry("blue-cellolose-fiber")
--- vgal.recipe.add_productivity_entry("crystal-slurry")
--- vgal.recipe.add_productivity_entry("slag-slurry")
--- vgal.recipe.add_productivity_entry("concrete")
-
--- allow productivity edge cases.
 
 -- CHEM
 vgal.recipe.smart_allow_productivity("angels-rocket-fuel-capsule")
@@ -127,17 +120,17 @@ vgal.recipe.smart_allow_productivity("angels-solid-ammonium-perchlorate")
 
 -- WATER
 vgal.recipe.smart_allow_productivity("angels-liquid-coolant")
-vgal.recipe.smart_allow_productivity("angels-water-saline")
-vgal.recipe.smart_allow_productivity("angels-solid-salt-dissolving")
-vgal.recipe.smart_allow_productivity("angels-solid-salt")
-vgal.recipe.smart_allow_productivity("angels-stone-crushed")
 
 -- BIO
 vgal.recipe.smart_allow_productivity("angels-fermentation-corn")
 vgal.recipe.smart_allow_productivity("angels-fermentation-fruit")
-vgal.recipe.smart_allow_productivity("angels-liquid-acetic-acid")
-vgal.recipe.smart_allow_productivity("angels-gas-ethanol")
+-- commentecbc; other (chem)path doesnt allow prod, might be unfair
+-- vgal.recipe.smart_allow_productivity("angels-liquid-acetic-acid")
+-- vgal.recipe.smart_allow_productivity("angels-gas-ethanol")
+
 vgal.recipe.smart_allow_productivity("angels-liquid-cellulose-acetate-mixture")
+vgal.recipe.smart_allow_productivity("angels-electrode")
+vgal.recipe.smart_allow_productivity("angels-paste-cellulose")
 
 vgal.recipe.smart_allow_productivity("angels-blue-cellulose-fiber")
 vgal.recipe.smart_allow_productivity("angels-red-cellulose-fiber")
@@ -157,9 +150,6 @@ vgal.recipe.smart_allow_productivity("angels-solid-saw")
 vgal.recipe.smart_allow_productivity("angels-solid-crystal-tipped-saw")
 vgal.recipe.smart_allow_productivity("angels-solid-crystal-full-saw")
 vgal.recipe.smart_allow_productivity("angels-alien-goo")
-vgal.recipe.smart_allow_productivity("angels-liquid-polluted-fish-atmosphere-raw-meat")
-vgal.recipe.smart_allow_productivity("angels-liquid-fish-atmosphere")
-vgal.recipe.smart_allow_productivity("angels-solid-sodium-carbonate") -- hmm
 vgal.recipe.smart_allow_productivity("angels-solid-calcium-carbonate")
 vgal.recipe.smart_allow_productivity("angels-gas-methanol-from-wood")
 vgal.recipe.smart_allow_productivity("angels-carbon-from-charcoal")
@@ -168,10 +158,6 @@ vgal.recipe.smart_allow_productivity("angels-wood-sawing-1")
 vgal.recipe.smart_allow_productivity("angels-wood-sawing-2")
 vgal.recipe.smart_allow_productivity("angels-wood-sawing-3")
 
-for puffer_index = 1, 5 do
-    vgal.recipe.add_catalyst_entry("angels-bio-puffer-" .. puffer_index)
-end
-
 -- FAUNA
 vgal.recipe.smart_allow_productivity("angels-gas-puffer-atmosphere")
 vgal.recipe.smart_allow_productivity("angels-biter-small-egg")
@@ -179,8 +165,13 @@ vgal.recipe.smart_allow_productivity("angels-biter-medium-egg")
 vgal.recipe.smart_allow_productivity("angels-biter-big-egg")
 vgal.recipe.smart_allow_productivity("angels-bio-alien-processed-meat")
 vgal.recipe.smart_allow_productivity("angels-petri-dish")
+vgal.recipe.smart_allow_productivity("angels-substrate-dish")
+vgal.recipe.smart_allow_productivity("angels-seeded-dish")
+vgal.recipe.smart_allow_productivity("angels-crystal-seed")
+vgal.recipe.smart_allow_productivity("angels-egg-shell-seed")
 vgal.recipe.smart_allow_productivity("angels-crystal-enhancer")
 vgal.recipe.smart_allow_productivity("angels-crystal-grindstone")
+vgal.recipe.smart_allow_productivity("angels-alien-spores")
 
 -- MINERALS
 vgal.recipe.smart_allow_productivity("angels-geode-blue-liquify")
@@ -194,9 +185,6 @@ vgal.recipe.smart_allow_productivity("angels-crystal-powder-slurry")
 vgal.recipe.smart_allow_productivity("angels-slag-processing-dissolution")
 vgal.recipe.smart_allow_productivity("angels-stone-crushed-dissolution")
 vgal.recipe.smart_allow_productivity("angels-liquid-concrete")
-vgal.recipe.smart_allow_productivity("angels-liquid-concrete-2")
--- vgal.recipe.smart_allow_productivity("angels-reinforced-concrete-brick")
-vgal.recipe.smart_allow_productivity("angels-concrete-brick")
 
 -- METALLURGY
 for _, metal in pairs(vgal.defines.metals --[[@as table<string, agal.Metal>]]) do
@@ -211,4 +199,31 @@ end
 data.raw["recipe"]["concrete"].allow_productivity = true
 data.raw["recipe"]["angels-nuclear-fuel"].allow_productivity = true
 data.raw["recipe"]["angels-nuclear-fuel-2"].allow_productivity = true
+data.raw["recipe"]["angels-milling-drum"].allow_productivity = true
+data.raw["recipe"]["angels-steam-cracking-oil-residual"].allow_productivity = true
+vgal.recipe.smart_allow_productivity("angels-filter-lime-used-cleaning")
+
+-- fixes
+vgal.recipe.allow_productivity_for_all_results("angels-gas-separation")
+vgal.recipe.allow_productivity_for_all_results("angels-oil-separation")
+
+-- PROD DISALLOW
+
+data.raw["recipe"]["angels-solid-salt-dissolving"].allow_productivity = false
+data.raw["recipe"]["angels-solid-salt"].allow_productivity = false
+data.raw["recipe"]["angels-water-saline-from-water"].allow_productivity = false
+data.raw["recipe"]["angels-concrete-brick"].allow_productivity = false
+data.raw["recipe"]["angels-clay-brick"].allow_productivity = false
+data.raw["recipe"]["nuclear-fuel-reprocessing"].allow_productivity = false
+data.raw["recipe"]["angels-coolant-used-filtration-1"].allow_productivity = false
+data.raw["recipe"]["angels-coolant-used-filtration-2"].allow_productivity = false
+vgal.recipe.disallow_productivity_for_result("angels-coke-purification-2", "angels-solid-sodium-carbonate")
+vgal.recipe.disallow_productivity_for_result("angels-coke-purification-2", "water")
+
+-- ADD PROD ENTRIES (that didnt get added with the smart_allow_productivity calls)
+
+vgal.recipe.add_productivity_entry("angels-thorium-ore")
+for _, metal in pairs(vgal.defines.metals --[[@as table<string, agal.Metal>]]) do
+    vgal.recipe.add_productivity_entry(metal.nugget)
+    vgal.recipe.add_productivity_entry(metal.slag)
 end

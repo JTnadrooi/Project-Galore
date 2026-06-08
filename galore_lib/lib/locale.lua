cc9681b8036ce7a94a9fd6905abe52b290505e65
JTnadrooi <jtnadrooi@gmail.com>
2026-06-08 20:05:25 +0200

    Add plant_result check to guess_locale

--- galore_lib/lib/locale.lua
+++ galore_lib/lib/locale.lua
@@ -8,7 +8,10 @@ function vgal.locale.guess_locale(prototype_name)
     if prototype_name:sub(-9) == "equipment" then return { "equipment-name." .. prototype_name } end
 
     local item = data.raw.item[prototype_name]
-    if item then return { (item.place_result and "entity" or "item") .. "-name." .. prototype_name } end
+    if item then
+        return { ((item.place_result and not item.plant_result) and "entity" or "item") ..
+        "-name." .. prototype_name }
+    end
 
     if data.raw.fluid[prototype_name] then return { "fluid-name." .. prototype_name } end
 

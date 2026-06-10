a5a8f60dcb631cd29e7eb25e5b5aeaf4408a7fc7
JTnadrooi <jtnadrooi@gmail.com>
2026-06-10 08:40:05 +0200

    Add pondering

--- angels_space_age_galore/overrides/autoplace-overrides.lua
+++ angels_space_age_galore/overrides/autoplace-overrides.lua
@@ -1,3 +1,7 @@
 -- move puffer nests to gleba
 data.raw["planet"]["nauvis"].map_gen_settings.autoplace_settings.entity.settings["angels-puffer-nest"] = nil
 data.raw["planet"]["gleba"].map_gen_settings.autoplace_settings.entity.settings["angels-puffer-nest"] = {}
+
+-- maybe add fluorite to gleba?
+-- might be fitting with how the puffers like it, but its already kinda aquilos thing
+-- importing it might be funny? would require some recipes to be reworked to make fluorite less qauntative (?)

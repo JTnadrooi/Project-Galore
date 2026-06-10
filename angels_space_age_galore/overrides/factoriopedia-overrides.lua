4402a29bd1b2586f4f1df79a345ed787b32ba35e
JTnadrooi <jtnadrooi@gmail.com>
2026-06-10 08:40:29 +0200

    Make puffer nest sim prettier

--- angels_space_age_galore/overrides/factoriopedia-overrides.lua
+++ angels_space_age_galore/overrides/factoriopedia-overrides.lua
@@ -1,11 +1,12 @@
 data.raw["tree"]["angels-puffer-nest"].factoriopedia_simulation = {
-    hide_factoriopedia_gradient = true,
-    init = [[
+  hide_factoriopedia_gradient = true,
+  planet = "gleba",
+  init = [[
     game.simulation.camera_zoom = 1.4
     game.simulation.camera_position = {-0.5, 0}
     for x = -10, 9, 1 do
       for y = -4, 4 do
-        game.surfaces[1].set_tiles{{position = {x, y}, name = "lowland-brown-blubber"}}
+        game.surfaces[1].set_tiles{{position = {x, y}, name = "midland-yellow-crust"}}
       end
     end
 

baa281ed5b3fef39d4090aaf926a01cebc7fbb81
JTnadrooi <jtnadrooi@gmail.com>
2026-06-11 08:55:51 +0200

    Add missing tech-overrides req

--- angels_space_age_galore/data-updates.lua
+++ angels_space_age_galore/data-updates.lua
@@ -1,6 +1,7 @@
 require("overrides.tech-overrides")
 require("overrides.overrides")
 require("overrides.recipe-overrides")
+require("overrides.tech-overrides")
 
 require("overrides.order-overrides")
 require("overrides.icon-overrides")

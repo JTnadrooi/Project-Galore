dc177fac6e535fbee7370052743cfd536d6c64a1
JTnadrooi <jtnadrooi@gmail.com>
2026-06-14 10:30:30 +0200

    Add stat display to desc and improve prod display with better newlines

--- galore_lib/data-final-fixes.lua
+++ galore_lib/data-final-fixes.lua
@@ -11,19 +11,52 @@ if settings.startup["vgal-show-prod-descs"].value then
             goto continue
         end
 
-        local ignored = {}
+        local ignored_prod = {}
+        local ignored_out_stats = {}
+        local ignored_in_stats = {}
+        for _, ingredient in ipairs(recipe.ingredients or {}) do
+            if ingredient.ignored_by_stats then
+                if not ingredient.amount or ingredient.ignored_by_stats < ingredient.amount then
+                    table.insert(ignored_in_stats, ingredient.name .. "(" .. ingredient.ignored_by_stats .. ")")
+                else
+                    table.insert(ignored_in_stats, ingredient.name)
+                end
+            end
+        end
         for _, result in ipairs(recipe.results or {}) do
             if result.ignored_by_productivity then
                 if not result.amount or result.ignored_by_productivity < result.amount then
-                    table.insert(ignored, result.name .. "(" .. result.ignored_by_productivity .. ")")
+                    table.insert(ignored_prod, result.name .. "(" .. result.ignored_by_productivity .. ")")
                 else
-                    table.insert(ignored, result.name)
+                    table.insert(ignored_prod, result.name)
+                end
+                if result.ignored_by_stats then
+                    if not result.amount or result.ignored_by_stats < result.amount then
+                        table.insert(ignored_out_stats, result.name .. "(" .. result.ignored_by_stats .. ")")
+                    else
+                        table.insert(ignored_out_stats, result.name)
+                    end
                 end
             end
         end
 
-        if #ignored > 0 then
-            table.insert(info_lines, "    Ignored: " .. table.concat(ignored, ", "))
+        if #ignored_prod > 0 then
+            table.insert(info_lines, "    Ignored(prod): ")
+            for _, item in ipairs(ignored_prod) do
+                table.insert(info_lines, "        " .. item .. ",")
+            end
+        end
+        if #ignored_in_stats > 0 then
+            table.insert(info_lines, "    Ignored(stats, in): ")
+            for _, item in ipairs(ignored_in_stats) do
+                table.insert(info_lines, "        " .. item .. ",")
+            end
+        end
+        if #ignored_out_stats > 0 then
+            table.insert(info_lines, "    Ignored(stats, out): ")
+            for _, item in ipairs(ignored_out_stats) do
+                table.insert(info_lines, "        " .. item .. ",")
+            end
         end
 
         recipe.localised_description = vgal.locale.bypass_max_locale_length(table.concat(info_lines, "\n"))

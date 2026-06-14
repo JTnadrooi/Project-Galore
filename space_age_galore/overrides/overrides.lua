f818ae924b421c4c69897cf74120f87822ee6d1a
JTnadrooi <jtnadrooi@gmail.com>
2026-06-14 10:46:59 +0200

    Fix bacteria surface_conditions

--- space_age_galore/overrides/overrides.lua
+++ space_age_galore/overrides/overrides.lua
@@ -11,6 +11,8 @@ auto_barrel("lithium-brine")
 auto_barrel("thruster-oxidizer")
 auto_barrel("thruster-fuel")
 
+data.raw["recipe"]["iron-bacteria-cultivation"].surface_conditions = nil
+data.raw["recipe"]["copper-bacteria-cultivation"].surface_conditions = nil
 data.raw["recipe"]["iron-bacteria"].surface_conditions = nil
 data.raw["recipe"]["copper-bacteria"].surface_conditions = nil
 

33bad5fe87a8b5c83735ecbf2ce2517cf30ba852
JTnadrooi <jtnadrooi@gmail.com>
2026-06-21 21:12:35 +0200

    Add vgal.recipe.make_recipeable_void(recipeable_name, void_category, void_amount)

--- angels_galore/prototypes/recipes/voids.lua
+++ angels_galore/prototypes/recipes/voids.lua
@@ -1,3 +1,3 @@
-angelsmods.functions.make_void("angels-liquid-polluted-fish-atmosphere", "water")
-angelsmods.functions.make_void("angels-liquid-fish-atmosphere", "water")
-angelsmods.functions.make_void("angels-tree-seed", "bio", 10)
+vgal.recipe.make_recipeable_void("angels-liquid-polluted-fish-atmosphere", "water")
+vgal.recipe.make_recipeable_void("angels-liquid-fish-atmosphere", "water")
+vgal.recipe.make_recipeable_void("angels-tree-seed", "bio", 10)

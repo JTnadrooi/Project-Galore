2656237f389c82fe3c1cc88db9280c2e53df12e9
JTnadrooi <jtnadrooi@gmail.com>
2026-06-21 16:48:21 +0200

    Add fish atm and tree seed void

--- angels_galore/prototypes/recipes/voids.lua
+++ angels_galore/prototypes/recipes/voids.lua
@@ -1,1 +1,3 @@
--- angelsmods.functions.make_void("angels-liquid-polluted-fish-atmosphere", "water")
+angelsmods.functions.make_void("angels-liquid-polluted-fish-atmosphere", "water")
+angelsmods.functions.make_void("angels-liquid-fish-atmosphere", "water")
+angelsmods.functions.make_void("angels-tree-seed", "bio", 10)

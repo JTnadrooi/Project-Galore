80fc2e2781663e5e6b87bbacd881de837750f834
JTnadrooi <jtnadrooi@gmail.com>
2026-06-22 13:09:00 +0200

    "angels-stone-crushed-angels-solid-carbon-stone" tweaks

--- angels_galore/prototypes/recipes/resources.lua
+++ angels_galore/prototypes/recipes/resources.lua
@@ -73,9 +73,9 @@ vgal.data.extend({
             vgal.icon.get_in("angels-solid-carbon"),
         },
         technology = { "angels-metallurgy-2", "angels-coal-processing" },
-        energy_required = 3.2,
+        energy_required = 2.4,
         ingredients = {
-            { "angels-stone-crushed", 8 },
+            { "angels-stone-crushed", 6 },
             { "angels-solid-carbon",  1 }, -- 6
         },
         results = {

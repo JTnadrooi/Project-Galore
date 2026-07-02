68e4f67f2234812cd76f83b6d38f1923b35ce079
JTnadrooi <jtnadrooi@gmail.com>
2026-07-02 09:52:35 +0200

    Add quality upgrade recipes

--- vanilla_galore_continued/prototypes/settings/other-settings.lua
+++ vanilla_galore_continued/prototypes/settings/other-settings.lua
@@ -13,3 +13,27 @@ data:extend {
     vgal.setting.new_string("vgal-custom-disabled-recipes", "aba"), -- only overrides groups.
     vgal.setting.new_string("vgal-custom-enabled-recipes", "abb"),  -- only overrides groups.
 }
+
+if mods["quality"] then
+    data:extend {
+        {
+            type = "string-setting",
+            name = "vgal-quality-upgrades-mode",
+            setting_type = "startup",
+            default_value = "Disabled",
+            allowed_values = {
+                "Disabled", "Replace", "Add"
+            },
+            order = "ca"
+        },
+        {
+            type = "double-setting",
+            name = "vgal-quality-upgrades-result-probability",
+            setting_type = "startup",
+            default_value = 1 / 4,
+            minimum_value = 0.01,
+            maximum_value = 1,
+            order = "cb"
+        },
+    }
+end

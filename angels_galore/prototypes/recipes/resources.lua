89c6d728647d711ac5e62011f60d1166ab59ebf9
JTnadrooi <jtnadrooi@gmail.com>
2026-08-27 20:27:45 +0200

    Reduce angels-solid-calcium-carbonate-angels-solid-lime carbon dioxide yield

--- angels_galore/prototypes/recipes/resources.lua
+++ angels_galore/prototypes/recipes/resources.lua
@@ -68,7 +68,7 @@ vgal.data.extend({
             { "angels-solid-lime", 4 },
         },
         fluid_results = {
-            { "angels-gas-carbon-dioxide", 60 },
+            { "angels-gas-carbon-dioxide", 50 },
         },
         category = "angels-blast-smelting",
         allow_productivity = false,

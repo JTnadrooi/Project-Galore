b1c668b6423acca8b8dfb443e8e75581d7be9b23
JTnadrooi <jtnadrooi@gmail.com>
2026-09-04 13:33:10 +0200

    Add biocoal-like recipes

--- angels_galore/prototypes/recipes/petrochem.lua
+++ angels_galore/prototypes/recipes/petrochem.lua
@@ -54,8 +54,75 @@ vgal.data.extend({
             "water",
             angelsmods.functions.fluid_color("NaCO3"),
         }),
-
     },
+    {
+        name = "angels-wood-pellets-angels-solid-coke",
+        prefix = "vgal",
+        icons = vgal.icon.register({
+            vgal.icon.get("angels-solid-coke"),
+            vgal.icon.get_in("angels-wood-pellets"),
+        }),
+        energy_required = 5,
+        technology = "angels-bio-wood-processing-2",
+        ingredients = {
+            { "angels-wood-pellets", 3 },
+        },
+        fluid_results = {
+            { "angels-liquid-condensates", 25 },
+        },
+        results = {
+            { "angels-solid-coke", 2 },
+        },
+        category = "angels-liquifying",
+
+        groups = { "vgal-unsure" }
+    },
+    {
+        name = "angels-wood-charcoal-angels-coal-crushed",
+        prefix = "vgal",
+        icons = vgal.icon.register({
+            vgal.icon.get("angels-coal-crushed"),
+            vgal.icon.get_in("angels-wood-charcoal"),
+        }),
+        energy_required = 6,
+        technology = "angels-bio-wood-processing-2",
+        ingredients = {
+            { "angels-wood-charcoal", 2 },
+            { "angels-coal-crushed",  6 },
+        },
+        results = {
+            { "angels-coal-crushed", 8 },
+        },
+        category = "angels-ore-refining-t1",
+
+        groups = { "vgal-unsure" }
+    },
+    -- {
+    --     name = "angels-solid-oil-residual-angels-solid-coke",
+    --     prefix = "vgal",
+    --     icons = vgal.icon.register({
+    --         vgal.icon.get("angels-solid-coke"),
+    --         vgal.icon.get_in("angels-solid-oil-residual"),
+    --     }),
+    --     energy_required = 2,
+    --     technology = "angels-bio-wood-processing-2",
+    --     fluid_ingredients = {
+    --         --     { "angels-liquid-vegetable-oil", 100 },
+    --         -- { "angels-gas-oxygen", 20 },
+    --     },
+    --     ingredients = {
+    --         -- { "angels-wood-charcoal", 2 },
+    --         { "angels-solid-oil-residual", 2 },
+    --     },
+    --     fluid_results = {
+    --         { "angels-gas-methane", 20 },
+    --         -- { "angels-water-purified", 30 },
+    --     },
+    --     results = {
+    --         { "angels-solid-coke", 5 },
+    --     },
+    --     category = "angels-liquifying",
+    -- },
 }, {
     type = "recipe",
 })

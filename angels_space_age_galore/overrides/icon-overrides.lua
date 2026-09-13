fe23687a742fcb71e154bb9c6880bd136ab3556d
JTnadrooi <jtnadrooi@gmail.com>
2026-09-13 08:02:17 +0200

    Finalize holmium plate icon

--- angels_space_age_galore/overrides/icon-overrides.lua
+++ angels_space_age_galore/overrides/icon-overrides.lua
@@ -3,8 +3,7 @@ if not mods["reskins-angels"] then
     data.raw["item"]["tungsten-plate"].icon = "__angelssmeltinggraphics__/graphics/icons/plate-titanium.png"
     data.raw["item"]["tungsten-plate"].icon_size = 32
 
-    data.raw["item"]["holmium-plate"].icon = "__angels_space_age_galore__/graphics/icons/plate-holmium.png"
-    data.raw["item"]["holmium-plate"].icon_size = 32
+    vgal.icon.set_icons(data.raw["item"]["holmium-plate"], vgal.icon.get("holmium-plate"))
 
     data.raw["item"]["lithium-plate"].icon = "__angelssmeltinggraphics__/graphics/icons/plate-silver.png"
     data.raw["item"]["lithium-plate"].icon_size = 32

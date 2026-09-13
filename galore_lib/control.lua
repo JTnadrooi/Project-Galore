0b4ba1dc5bb6d1e257d90e4a9fc877a2d35d7f90
JTnadrooi <jtnadrooi@gmail.com>
2026-09-13 16:42:56 +0200

    Move "vgal-auto-research-tech-nodes" to galorelib

--- galore_lib/control.lua
+++ galore_lib/control.lua
@@ -1,4 +1,4 @@
-function vgal_reload_recipes(event)
+local function reload_recipes(event)
     for _, force in pairs(game.forces) do
         for _, tech in pairs(force.technologies) do
             if tech.researched then
@@ -12,11 +12,49 @@ function vgal_reload_recipes(event)
     end
 end
 
-script.on_init(vgal_reload_recipes)
-script.on_configuration_changed(vgal_reload_recipes)
+local function research_tech_nodes(event)
+    local forces = {}
+
+    if event.research and event.research.force then
+        table.insert(forces, event.research.force)
+    else
+        for _, force in pairs(game.forces) do
+            table.insert(forces, force)
+        end
+    end
+
+    for _, force in ipairs(forces) do
+        for _, tech in pairs(force.technologies) do
+            if tech.researched then
+                goto not_this_tech
+            end
+            for _, prerequisite in pairs(tech.prerequisites) do
+                if not prerequisite.researched then
+                    goto not_this_tech
+                end
+            end
+            if not string.match(tech.name, "^vgal-.*node[0-9]$") then
+                goto not_this_tech
+            end
+
+            tech.researched = true
+
+            ::not_this_tech::
+        end
+    end
+end
+
+script.on_init(reload_recipes)
+script.on_configuration_changed(reload_recipes)
 
 commands.add_command(
     "recipes-reload",
     "Reload all recipes. (This happens automatically.)",
-    vgal_reload_recipes
+    reload_recipes
 )
+
+if settings.startup["vgal-auto-research-tech-nodes"].value then
+    script.on_init(research_tech_nodes)
+    script.on_configuration_changed(research_tech_nodes)
+    script.on_event(defines.events.on_research_finished, research_tech_nodes)
+end
--- galore_lib/settings.lua
+++ galore_lib/settings.lua
@@ -3,4 +3,5 @@ require("lib.setting")
 data:extend {
     vgal.setting.new_bool("vgal-log", "a"),
     vgal.setting.new_bool("vgal-show-prod-descs", "b", false),
+    vgal.setting.new_bool("vgal-auto-research-tech-nodes", "c", false),
 }
